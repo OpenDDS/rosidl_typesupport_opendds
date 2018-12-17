f2f00612d0f2176db44751ee7eae775f20294286
Johnny Willemsen <jwillemsen@remedy.nl>
2018-12-17 13:09:11 +0100

    Import more from connext
        * rosidl_typesupport_opendds_cpp/rosidl_typesupport_opendds_cpp-extras.cmake:

--- rosidl_typesupport_opendds_cpp/rosidl_typesupport_opendds_cpp-extras.cmake
+++ rosidl_typesupport_opendds_cpp/rosidl_typesupport_opendds_cpp-extras.cmake
@@ -1,4 +1,20 @@
+# generated from
+# rosidl_typesupport_opendds_cpp/rosidl_typesupport_opendds_cpp-extras.cmake
+
+find_package(opendds_cmake_module QUIET)
 find_package(ament_cmake REQUIRED)
-find_package(opendds_cmake_module REQUIRED)
-find_package(OpenDDS MODULE)
-list(INSERT CMAKE_MODULE_PATH 0 "${rosidl_typesupport_opendds_cpp_DIR}/Modules")
+if(NOT OpenDDS_FOUND)
+  message(STATUS
+    "Could not find OpenDDS - skipping rosidl_typesupport_opendds_cpp")
+else()
+  find_package(ament_cmake_core QUIET REQUIRED)
+  ament_register_extension(
+    "rosidl_generate_interfaces"
+    "rosidl_typesupport_opendds_cpp"
+    "rosidl_typesupport_opendds_cpp_generate_interfaces.cmake")
+
+  set(rosidl_typesupport_opendds_cpp_BIN
+    "${rosidl_typesupport_opendds_cpp_DIR}/../../../lib/rosidl_typesupport_opendds_cpp/rosidl_typesupport_opendds_cpp")
+  normalize_path(rosidl_typesupport_opendds_cpp_BIN
+    "${rosidl_typesupport_opendds_cpp_BIN}")
+endif()
