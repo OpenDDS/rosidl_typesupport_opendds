1b820e8fcb2774237d11664a8e7f3b28a839dd9d
Johnny Willemsen <jwillemsen@remedy.nl>
2018-12-17 12:57:41 +0100

    Taking over more content from connext
        * rosidl_typesupport_opendds_c/rosidl_typesupport_opendds_c-extras.cmake:

--- rosidl_typesupport_opendds_c/rosidl_typesupport_opendds_c-extras.cmake
+++ rosidl_typesupport_opendds_c/rosidl_typesupport_opendds_c-extras.cmake
@@ -1,9 +1,17 @@
-find_package(ament_cmake REQUIRED)
-find_package(opendds_cmake_module REQUIRED)
-find_package(OpenDDS MODULE)
-list(INSERT CMAKE_MODULE_PATH 0 "${rosidl_typesupport_opendds_c_DIR}/Modules")
+find_package(opendds_cmake_module QUIET)
+find_package(OpenDDS QUIET MODULE)
+if(NOT OpenDDS_FOUND)
+  message(STATUS
+    "Could not find OpenDDS - skipping rosidl_typesupport_opendds_c")
+else()
+  find_package(ament_cmake_core QUIET REQUIRED)
+  ament_register_extension(
+      "rosidl_generate_interfaces"
+      "rosidl_typesupport_opendds_c"
+      "rosidl_typesupport_opendds_c_generate_interfaces.cmake")
 
-ament_register_extension(
-    "rosidl_generate_interfaces"
-    "rosidl_typesupport_opendds_c"
-    "rosidl_typesupport_opendds_c_generate_interfaces.cmake")
+  set(rosidl_typesupport_opendds_c_BIN
+    "${rosidl_typesupport_opendds_c_DIR}/../../../lib/rosidl_typesupport_opendds_c/rosidl_typesupport_opendds_c")
+  normalize_path(rosidl_typesupport_opendds_c_BIN
+    "${rosidl_typesupport_opendds_c_BIN}")
+endif()
