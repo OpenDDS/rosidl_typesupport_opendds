8be4246a98b78647ebefc561e9c068f387e93e9a
Mike Kuznetsov <kuznetsovm@objectcomputing.com>
2020-05-01 16:29:33 -0500

    Mods to RequesterParams

--- rosidl_typesupport_opendds_cpp/include/rosidl_typesupport_opendds_cpp/requester_parameters.h
+++ rosidl_typesupport_opendds_cpp/include/rosidl_typesupport_opendds_cpp/requester_parameters.h
@@ -5,22 +5,104 @@
 #include "dds/DCPS/Service_Participant.h"
 #include "dds/DCPS/Marked_Default_Qos.h"
 
-  struct RequesterParams {
+  class RequesterParams {
+  public:
+    RequesterParams()
+      : participant_(nullptr) {
+    };
 
     RequesterParams(DDS::DomainParticipant* participant) {
       participant_ = participant;
-      // TODO: Create a RequesterParams with the parameters a Requester always needs
+      // TODO(?): Create a RequesterParams with the parameters a Requester always needs
     };
 
+    /*
+    SET
+    */
+    RequesterParams& domain_participant(DDS::DomainParticipant* participant) {
+      participant_ = participant;
+      return *this;
+    };
 
+    RequesterParams& publisher(DDS::Publisher_var publisher) {
+      dds_publisher_ = publisher;
+      return *this;
+    };
+
+    RequesterParams& subscriber(DDS::Subscriber_var subscriber) {
+      dds_subscriber_ = subscriber;
+      return *this;
+    };
+
+    RequesterParams& datawriter_qos(DDS::DataWriterQos qos) {
+      dw_qos_ = qos;
+      return *this;
+    };
+
+    RequesterParams& datareader_qos(DDS::DataReaderQos qos) {
+      dr_qos_ = qos;
+      return *this;
+    };
+
+    RequesterParams& service_name(const std::string& name) {
+      service_name_ = name;
+      return *this;
+    };
+
+    RequesterParams& request_topic_name(const std::string& name) {
+      request_topic_name_ = name;
+      return *this;
+    };
+
+    RequesterParams& reply_topic_name(const std::string& name) {
+      reply_topic_name_ = name;
+      return *this;
+    };
+
+    /*
+    GET
+    */
+    DDS::DomainParticipant* domain_participant() const {
+      return participant_;
+    };
+
+    DDS::Publisher_var publisher() const {
+      return dds_publisher_;
+    };
+
+    DDS::Subscriber_var subscriber() const {
+      return dds_subscriber_;
+    };
+
+    DDS::DataWriterQos datawriter_qos() const {
+      return dw_qos_;
+    };
+
+    DDS::DataReaderQos datareader_qos() const {
+      return dr_qos_;
+    };
+
+    std::string service_name() const {
+      return service_name_;
+    };
+
+    std::string request_topic_name() const {
+      return request_topic_name_;
+    };
+
+    std::string reply_topic_name() const {
+      return reply_topic_name_;
+    };
+
+  private:
     DDS::DomainParticipant* participant_;
+    DDS::Publisher_var dds_publisher_;
+    DDS::Subscriber_var dds_subscriber_;
     DDS::DataWriterQos dw_qos_;
     DDS::DataReaderQos dr_qos_;
     std::string service_name_;
     std::string request_topic_name_;
     std::string reply_topic_name_;
-    DDS::Publisher_var dds_publisher_;
-    DDS::Subscriber_var dds_subscriber_;
   };
 
 
